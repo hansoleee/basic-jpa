dfef9d7cb10110e8ffea8ab31d209ab6674c7731
lhs <leehansol.kr@gmail.com>
2021-09-26 14:01:22 +0900

    Member 저장

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,8 +1,33 @@
 package hellojpa;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 public class JpaMain {
 
     public static void main(String[] args) {
+        final EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
+        final EntityManager em = emf.createEntityManager();
+
+        final EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setId(2L);
+            member.setName("lhb");
+
+            em.persist(member);
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
     }
 }
